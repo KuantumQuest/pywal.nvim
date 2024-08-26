af66c89170bb9179c058dc0f8eb0c217830a8638
KuantumQuest <83140383+KuantumQuest@users.noreply.github.com>
2024-08-26 09:11:21 +0200

    add colors of Tresitter-context to init.lua

--- lua/pywal/init.lua
+++ lua/pywal/init.lua
@@ -6,6 +6,15 @@ function M.setup ()
   local colors = core.get_colors()
   vim.opt.termguicolors = true
   highlights.highlight_all(colors)
+  -- Treesitter context
+  -- Configura el resaltado para TreesitterContext
+  --vim.api.nvim_set_hl(0, "TreesitterContext", { bg = colores.color1 })
+  -- Configura el resaltado para los números de línea en el contexto Treesitter
+  vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg = colores.color4 })
+  -- Configura el resaltado para el separador si es necesario
+  vim.api.nvim_set_hl(0, "TreesitterContextSeparator", { fg = colores.grey, bg = colores.foreground })
+  -- Para la última línea del contexto Treesitter
+  vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = colores.foreground })
 end
 
 return M
